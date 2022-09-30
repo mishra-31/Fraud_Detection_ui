f25d33848ad89a074ba0ad6f5c34302c491dd308
Abhishek Mishra <82857542+mishra-31@users.noreply.github.com>
2022-09-30 20:50:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,10 +44,10 @@ if(st.button('Submit')):
     resp = requests.post(url, headers=headers, data=data)
     st.write(resp.json())
     
-    time.sleep(10)
+    time.sleep(5)
     ID=resp.json()['ID']
     alarm_url = "https://api-io.waylay.io/alarms/v1/alarms?source=" + ID
-    
+    time.sleep(5)
     resp2 = requests.get(alarm_url, auth=HTTPBasicAuth('e10a0f641bccdafbd954a29a', 'tiUG/cuntOmZN8Qyb/sTckuImIstdM5K'))
 
     
